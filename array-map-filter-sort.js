3b431350fef488e50cf1c5f60a8fa27a299060af
lolkiki25 <25LP2625@nest.edu.mn>
2025-07-30 12:21:25 +0800

    l

--- array-map-filter-sort.js
+++ array-map-filter-sort.js
@@ -97,11 +97,92 @@ students.sort((student2, student1) => {
 });
 console.log(students);
 
-// nasand hursen suragchiig filterlej oloh function bich
-// gender ogonguut tuhain gendereer filterlej ogoh function bich
-// eg: filterByGender(students,"male") => zovhon erchuudiig yalgaj ogno
-// nasaar n sortloh function bich
+
+
+
 // neg too ogonguut tuhain toonoos ih balanactei surgchdiig yalgaj ogoh function bich
+let data4 = [
+  {
+    name: "boldo",
+    age: 3,
+    grade: 11,
+    gender: "male",
+    balance: 1231,
+  },
+  {
+    name: "dorjo",
+    age: 20,
+    grade: 11,
+    gender: "male",
+    balance: 33,
+  },
+  {
+    name: "tsetsge",
+    age: 10,
+    grade: 11,
+    gender: "female",
+    balance: 222,
+  },
+  {
+    name: "dulma",
+    age: 9,
+    grade: 11,
+    gender: "female",
+    balance: 12,
+  },
+];
+function findByBalance(data4, balance) {
+  let filtered = data4.filter((student) => {
+    return student.balance > balance;
+  });
+  return filtered;
+}
+let du = findByBalance(data4, 222);
+console.log("bodlog 4 = ", du);
+// classCode gesen field nemeh function bich
+let data5 = [
+  {
+    name: "boldo",
+    age: 3,
+    grade: 11,
+    gender: "male",
+    balance: 1231,
+  },
+  {
+    name: "dorjo",
+    age: 20,
+    grade: 11,
+    gender: "male",
+    balance: 33,
+  },
+  {
+    name: "tsetsge",
+    age: 10,
+    grade: 11,
+    gender: "female",
+    balance: 222,
+  },
+  {
+    name: "dulma",
+    age: 9,
+    grade: 11,
+    gender: "female",
+    balance: 12,
+  },
+];
+function findByBalance(data5) {
+  let filtered = data5.map((student) => {
+    return {
+      classCode = 
+        }
+  });
+  return filtered;
+}
+let dulm = findByBalance(data5, 222);
+console.log("bodlog 4 = ", dulm);
+// eg: addClassCodeToStudents(students,"3A") => [{name: "boldo",age: 3,grade: 11,gender: "male",balance: 1231,classCode:"3A"},...]
+// removeGenders from student array function bich
+//noos ih balanactei surgchdiig yalgaj ogoh function bich
 // classCode gesen field nemeh function bich
 // eg: addClassCodeToStudents(students,"3A") => [{name: "boldo",age: 3,grade: 11,gender: "male",balance: 1231,classCode:"3A"},...]
 // removeGenders from student array function bich
@@ -142,10 +223,10 @@ let data = [
 ];
 
 function findAdultStudents(data) {
-    let filtered = data.filter((student) => {
-        return student.age > 18;
-    });
-    return filtered;
+  let filtered = data.filter((student) => {
+    return student.age > 18;
+  });
+  return filtered;
 }
 let result = findAdultStudents(data);
 console.log("bodlog 1 = ", result);
@@ -183,10 +264,10 @@ let data2 = [
   },
 ];
 function findByGender(data2, gender) {
-const filtereddata = data2.filter((student) => {
-        return student.gender === gender;
-    });
-    return filtereddata;
+  const filtereddata = data2.filter((student) => {
+    return student.gender === gender;
+  });
+  return filtereddata;
 }
 let result1 = findByGender(data2, "male");
 console.log(result1);
@@ -225,10 +306,511 @@ let data3 = [
   },
 ];
 function findAgeStudents(data3, age) {
-    let ah = ages.sort((age2, age1) => {
-  return age2 - age1 ;
-});
-return ah;
-}
+  let ah = ages.sort((age2, age1) => {
+    return age2 - age1;
+  });
+  return ah;
+};
 let result2 = findAgeStudents(data3, 20);
 console.log(result2);
+// Нэг л products массив дээр ажиллана
+// 1. Хямдралтай бүтээгдэхүүнүүдийг шүүж буцаадаг функц бич.
+
+let data9 = [
+  {
+    name: "iPhone 13",
+    category: "Electronics",
+    price: 1200000,
+    stock: 10,
+    brand: "Apple",
+    rating: 4.8,
+    isDiscounted: true,
+    discountPercent: 10,
+    supplier: "MobiCom",
+    weight: 0.3,
+  },
+  {
+    name: "Samsung Galaxy S22",
+    category: "Electronics",
+    price: 950000,
+    stock: 5,
+    brand: "Samsung",
+    rating: 4.5,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "Unitel",
+    weight: 0.28,
+  },
+  {
+    name: "Dell XPS 13",
+    category: "Computers",
+    price: 2800000,
+    stock: 3,
+    brand: "Dell",
+    rating: 4.7,
+    isDiscounted: true,
+    discountPercent: 15,
+    supplier: "PCMall",
+    weight: 1.2,
+  },
+  {
+    name: "Mouse Logitech M590",
+    category: "Accessories",
+    price: 85000,
+    stock: 20,
+    brand: "Logitech",
+    rating: 4.3,
+    isDiscounted: true,
+    discountPercent: 5,
+    supplier: "PCMall",
+    weight: 0.1,
+  },
+  {
+    name: "MacBook Air M2",
+    category: "Computers",
+    price: 3200000,
+    stock: 0,
+    brand: "Apple",
+    rating: 4.9,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "MobiCom",
+    weight: 1.1,
+  },
+];
+function getDiscountedProducts(data9) {
+  const filtereddata = data9.filter((products) => {
+    return products.stock === 0;
+  });
+  return filtereddata;
+}
+let result11 = getDiscountedProducts(data9);
+console.log(result11);
+
+// 3. Үнэ нь 1 сая төгрөгөөс дээш бүтээгдэхүүнүүдийг буцаадаг функц бич.
+let data13 = [
+  {
+    name: "iPhone 13",
+    category: "Electronics",
+    price: 1200000,
+    stock: 10,
+    brand: "Apple",
+    rating: 4.8,
+    isDiscounted: true,
+    discountPercent: 10,
+    supplier: "MobiCom",
+    weight: 0.3,
+  },
+  {
+    name: "Samsung Galaxy S22",
+    category: "Electronics",
+    price: 950000,
+    stock: 5,
+    brand: "Samsung",
+    rating: 4.5,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "Unitel",
+    weight: 0.28,
+  },
+  {
+    name: "Dell XPS 13",
+    category: "Computers",
+    price: 2800000,
+    stock: 3,
+    brand: "Dell",
+    rating: 4.7,
+    isDiscounted: true,
+    discountPercent: 15,
+    supplier: "PCMall",
+    weight: 1.2,
+  },
+  {
+    name: "Mouse Logitech M590",
+    category: "Accessories",
+    price: 85000,
+    stock: 20,
+    brand: "Logitech",
+    rating: 4.3,
+    isDiscounted: true,
+    discountPercent: 5,
+    supplier: "PCMall",
+    weight: 0.1,
+  },
+  {
+    name: "MacBook Air M2",
+    category: "Computers",
+    price: 3200000,
+    stock: 0,
+    brand: "Apple",
+    rating: 4.9,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "MobiCom",
+    weight: 1.1,
+  },
+];
+function getExpensiveProducts(data13) {
+  const filtereddata = data13.filter((products) => {
+    return products.price > 1000000;
+  });
+  return filtereddata;
+}
+let result10 = getExpensiveProducts(data13);
+console.log(result10);
+
+
+// 4. "Computers" категорийн бүтээгдэхүүнүүдийг буцаадаг функц бич.
+
+let data11 = [
+  {
+    name: "iPhone 13",
+    category: "Electronics",
+    price: 1200000,
+    stock: 10,
+    brand: "Apple",
+    rating: 4.8,
+    isDiscounted: true,
+    discountPercent: 10,
+    supplier: "MobiCom",
+    weight: 0.3,
+  },
+  {
+    name: "Samsung Galaxy S22",
+    category: "Electronics",
+    price: 950000,
+    stock: 5,
+    brand: "Samsung",
+    rating: 4.5,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "Unitel",
+    weight: 0.28,
+  },
+  {
+    name: "Dell XPS 13",
+    category: "Computers",
+    price: 2800000,
+    stock: 3,
+    brand: "Dell",
+    rating: 4.7,
+    isDiscounted: true,
+    discountPercent: 15,
+    supplier: "PCMall",
+    weight: 1.2,
+  },
+  {
+    name: "Mouse Logitech M590",
+    category: "Accessories",
+    price: 85000,
+    stock: 20,
+    brand: "Logitech",
+    rating: 4.3,
+    isDiscounted: true,
+    discountPercent: 5,
+    supplier: "PCMall",
+    weight: 0.1,
+  },
+  {
+    name: "MacBook Air M2",
+    category: "Computers",
+    price: 3200000,
+    stock: 0,
+    brand: "Apple",
+    rating: 4.9,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "MobiCom",
+    weight: 1.1,
+  },
+];
+function getComputers(data11, category) {
+  const filtered = data11.filter((products) => {
+    return products.category === category;
+  });
+  return filtered;
+}
+let nomio = getComputers(data11, "Computers");
+console.log(nomio);
+// 5. Тухайн брэндийн нэрээр бүтээгдэхүүнүүдийг шүүдэг функц бич.
+let data12 = [
+  {
+    name: "iPhone 13",
+    category: "Electronics",
+    price: 1200000,
+    stock: 10,
+    brand: "Apple",
+    rating: 4.8,
+    isDiscounted: true,
+    discountPercent: 10,
+    supplier: "MobiCom",
+    weight: 0.3,
+  },
+  {
+    name: "Samsung Galaxy S22",
+    category: "Electronics",
+    price: 950000,
+    stock: 5,
+    brand: "Samsung",
+    rating: 4.5,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "Unitel",
+    weight: 0.28,
+  },
+  {
+    name: "Dell XPS 13",
+    category: "Computers",
+    price: 2800000,
+    stock: 3,
+    brand: "Dell",
+    rating: 4.7,
+    isDiscounted: true,
+    discountPercent: 15,
+    supplier: "PCMall",
+    weight: 1.2,
+  },
+  {
+    name: "Mouse Logitech M590",
+    category: "Accessories",
+    price: 85000,
+    stock: 20,
+    brand: "Logitech",
+    rating: 4.3,
+    isDiscounted: true,
+    discountPercent: 5,
+    supplier: "PCMall",
+    weight: 0.1,
+  },
+  {
+    name: "MacBook Air M2",
+    category: "Computers",
+    price: 3200000,
+    stock: 0,
+    brand: "Apple",
+    rating: 4.9,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "MobiCom",
+    weight: 1.1,
+  },
+];
+function filterByBrand(data12, brand) {
+  const filtereddata = data12.filter((products) => {
+    return products.brand === brand;
+  });
+  return filtereddata;
+}
+let result12 = filterByBrand(data12, "Logitech");
+console.log(result12);
+
+
+// 6. Бүх бүтээгдэхүүн дээр `isHeavy: true/false` гэсэн талбар нэмдэг функц бич (жин 1 кг-аас их бол).
+function addIsHeavyFlag(products) {
+  ...
+}
+
+// 7. Бүх бүтээгдэхүүний үнэд 10% нэмдэг функц бич.
+function increasePriceByTenPercent(products) {
+  // ...
+}
+
+// 8. Хямдралын дараах үнэтэй `finalPrice` талбарыг тооцож нэмдэг функц бич.
+function addFinalPrice(products) {
+  // ...
+}
+
+// 9. Үнэлгээ хамгийн өндөртэй бүтээгдэхүүнийг буцаадаг функц бич.
+function getTopRatedProduct(products) {
+  // ...
+}
+
+// 10. Үнэ хамгийн бага бүтээгдэхүүнийг буцаадаг функц бич.
+let data14 = [
+  {
+    name: "iPhone 13",
+    category: "Electronics",
+    price: 1200000,
+    stock: 10,
+    brand: "Apple",
+    rating: 4.8,
+    isDiscounted: true,
+    discountPercent: 10,
+    supplier: "MobiCom",
+    weight: 0.3,
+  },
+  {
+    name: "Samsung Galaxy S22",
+    category: "Electronics",
+    price: 950000,
+    stock: 5,
+    brand: "Samsung",
+    rating: 4.5,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "Unitel",
+    weight: 0.28,
+  },
+  {
+    name: "Dell XPS 13",
+    category: "Computers",
+    price: 2800000,
+    stock: 3,
+    brand: "Dell",
+    rating: 4.7,
+    isDiscounted: true,
+    discountPercent: 15,
+    supplier: "PCMall",
+    weight: 1.2,
+  },
+  {
+    name: "Mouse Logitech M590",
+    category: "Accessories",
+    price: 85000,
+    stock: 20,
+    brand: "Logitech",
+    rating: 4.3,
+    isDiscounted: true,
+    discountPercent: 5,
+    supplier: "PCMall",
+    weight: 0.1,
+  },
+  {
+    name: "MacBook Air M2",
+    category: "Computers",
+    price: 3200000,
+    stock: 0,
+    brand: "Apple",
+    rating: 4.9,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "MobiCom",
+    weight: 1.1,
+  },
+];
+function getCheapestProduct(data14) {
+  const filtera = data14.map((products) => {
+    return products.price >= price;
+  });
+  return filtera;
+}
+let nomt = getCheapestProduct(data14);
+console.log(nomt);
+
+// 11. Бүх бүтээгдэхүүний нийт нөөц (stock)-ийг тооцдог функц бич.
+let data14 = [
+  {
+    name: "iPhone 13",
+    category: "Electronics",
+    price: 1200000,
+    stock: 10,
+    brand: "Apple",
+    rating: 4.8,
+    isDiscounted: true,
+    discountPercent: 10,
+    supplier: "MobiCom",
+    weight: 0.3,
+  },
+  {
+    name: "Samsung Galaxy S22",
+    category: "Electronics",
+    price: 950000,
+    stock: 5,
+    brand: "Samsung",
+    rating: 4.5,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "Unitel",
+    weight: 0.28,
+  },
+  {
+    name: "Dell XPS 13",
+    category: "Computers",
+    price: 2800000,
+    stock: 3,
+    brand: "Dell",
+    rating: 4.7,
+    isDiscounted: true,
+    discountPercent: 15,
+    supplier: "PCMall",
+    weight: 1.2,
+  },
+  {
+    name: "Mouse Logitech M590",
+    category: "Accessories",
+    price: 85000,
+    stock: 20,
+    brand: "Logitech",
+    rating: 4.3,
+    isDiscounted: true,
+    discountPercent: 5,
+    supplier: "PCMall",
+    weight: 0.1,
+  },
+  {
+    name: "MacBook Air M2",
+    category: "Computers",
+    price: 3200000,
+    stock: 0,
+    brand: "Apple",
+    rating: 4.9,
+    isDiscounted: false,
+    discountPercent: 0,
+    supplier: "MobiCom",
+    weight: 1.1,
+  },
+];
+function getTotalStock(data14) {
+  const filtera = data14.filter((products) => {
+    return products.price += price;
+  });
+  return filtera;
+}
+let nomt = getCheapestProduct(data14);
+console.log(nomt);
+
+
+// 12. Бүх бүтээгдэхүүний нийлбэр үнийг тооцдог функц бич.
+function getTotalPrice(products) {
+  // ...
+}
+
+// 13. Тухайн supplier-аар шүүж бүтээгдэхүүнүүдийг буцаадаг функц бич.
+function filterBySupplier(products, supplierName) {
+  // ...
+}
+
+// 14. Бүх бүтээгдэхүүний нэрсийг массив болгон буцаадаг функц бич.
+function getProductNames(products) {
+  // ...
+}
+
+// 15. Бүтээгдэхүүнүүдийг үнээр нь өсөхөөр эрэмбэлж буцаадаг функц бич.
+function sortByPriceAscending(products) {
+  // ...
+}
+
+// 16. Нөөц багатай (≤ 5) бүтээгдэхүүнүүдийг шүүж буцаадаг функц бич.
+function getLowStockProducts(products) {
+  // ...
+}
+
+// 17. Давхардалгүй нийлүүлэгчийн нэрсийн массив буцаадаг функц бич.
+function getUniqueSuppliers(products) {
+  // ...
+}
+
+// 18. Зөвхөн name ба price талбартай шинэ массив үүсгэдэг функц бич.
+function getNameAndPriceList(products) {
+  // ...
+}
+
+// 19. Үнэлгээ нь 4.5-аас их бүх бүтээгдэхүүнүүдийг буцаадаг функц бич.
+function getHighlyRatedProducts(products) {
+  // ...
+}
+
+// 20. Бүх бүтээгдэхүүнд `id` талбар нэмж өгдөг функц бич (жишээ нь 1, 2, 3...).
+function addIdToProducts(products) {
+  // ...
+}
